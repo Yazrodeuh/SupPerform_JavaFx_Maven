fb0bb1218932583121af47fc45c2e38620f500df
Maxime <maxime.poujol01@etu.umontpellier.fr>
2021-01-19 09:44:40 +0100

    Stackpane

--- src/main/java/fr/montpellier/supperform/javaFX/StackPaneFX.java
+++ src/main/java/fr/montpellier/supperform/javaFX/StackPaneFX.java
@@ -1,22 +1,20 @@
 package fr.montpellier.supperform.javaFX;
 
-import fr.montpellier.supperform.Main;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 
-public class SceneFX extends Scene {
+public class StackPaneFX extends StackPane {
 
-    public SceneFX(Parent parent) {
-        super(new StackPaneFX(parent), Main.WIDTH, Main.HEIGHT);
-        this.setFill(Color.LIGHTGRAY);
-        this.getStylesheets().add("/Button.css");
+    public StackPaneFX(Parent parent){
+        this.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, Insets.EMPTY)));
+        this.getChildren().addAll(parent);
     }
+
 
 
 }
